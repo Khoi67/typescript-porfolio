a8ef7c95b431d2a53cfaee3adbe04dca97c51e52
Khoi67 <khoi672001@gmail.com>
2024-10-29 14:59:36 +0700

    Set up SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Portfolio from "./screens/portfolio";
-
+import "./styles/plugins.scss"
+import "./styles/style.scss"
 function App() {
   return (
     <Portfolio />
