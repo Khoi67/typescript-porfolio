835ce3a0a7695e9a3273f146973b866683282958
Khoi67 <khoi672001@gmail.com>
2024-11-07 22:53:28 +0700

    Remove Jquery

--- src/components/leftpart.tsx
+++ src/components/leftpart.tsx
@@ -1,9 +1,19 @@
 import desktopLogo from "@/assets/img/logo/desktop-logo.png";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
-const LeftPart = () => {
+interface IProps {
+  hideLeftPart: boolean;
+  setHideLeftPart: (value: boolean) => void;
+}
+const LeftPart = (props: IProps) => {
   return (
     <>
-      <div className="arlo_tm_leftpart_wrap">
+      <div
+        className={
+          props.hideLeftPart === true
+            ? "arlo_tm_leftpart_wrap opened"
+            : "arlo_tm_leftpart_wrap "
+        }
+      >
         <div className="leftpart_inner">
           <div className="logo_wrap">
             <a href="#">
@@ -33,25 +43,43 @@ const LeftPart = () => {
             <div className="social_wrap">
               <ul>
                 <li>
-                  <a href="https://www.facebook.com/khoi.donguyen.792" target="_blank">
+                  <a
+                    href="https://www.facebook.com/khoi.donguyen.792"
+                    target="_blank"
+                  >
                     <FaFacebook />
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.linkedin.com/in/nguy%C3%AAn-kh%C3%B4i-26032320b/" target="_blank">
+                  <a
+                    href="https://www.linkedin.com/in/nguy%C3%AAn-kh%C3%B4i-26032320b/"
+                    target="_blank"
+                  >
                     <FaLinkedin />
                   </a>
                 </li>
                 <li>
                   <a href="https://github.com/Khoi67" target="_blank">
-                  <FaGithub />
+                    <FaGithub />
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <a className="arlo_tm_resize" href="#">
-            <i className="xcon-angle-left"></i>
+          <a
+            className={
+              props.hideLeftPart ? "arlo_tm_resize opened" : "arlo_tm_resize"
+            }
+            href="#"
+            onClick={() => props.setHideLeftPart(!props.hideLeftPart)} //toggle
+          >
+            <i
+              className={
+                props.hideLeftPart
+                  ? "xcon-angle-left opened"
+                  : "xcon-angle-left"
+              }
+            ></i>
           </a>
         </div>
       </div>
--- src/components/rightpart.tsx
+++ src/components/rightpart.tsx
@@ -4,10 +4,14 @@ import HomeCV from "./content/homecv";
 import Project from "./content/project";
 import Skills from "./content/skills";
 
-const RightPart = () => {
+interface IProps {
+  hideLeftPart: boolean;
+  setHideLeftPart: (value: boolean) => void;
+}
+const RightPart = (props: IProps) => {
     return (
         <>
-        <div className="arlo_tm_rightpart">
+        <div className={props.hideLeftPart ? "arlo_tm_rightpart opened" : "arlo_tm_rightpart"}>
             <div className="rightpart_inner">
               <HomeCV />
               {/* <!-- ABOUT --> */}
