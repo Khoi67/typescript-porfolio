ae7e22849a7904268b1ad535fb56893530ef9e2f
Khoi67 <khoi672001@gmail.com>
2024-11-07 23:25:10 +0700

    Add Slug

--- src/components/leftpart.tsx
+++ src/components/leftpart.tsx
@@ -1,7 +1,34 @@
 import desktopLogo from "@/assets/img/logo/desktop-logo.png";
+import { useEffect, useState } from "react";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const LeftPart = () => {
+  const [activeTab, setActiveTab] = useState<string>("home");
+  useEffect(() => {
+    const { hash } = window.location;
+    if (hash) {
+      const tab = hash.replace("#", "");
+      setActiveTab(tab);
+      const section = document.querySelector(`${hash}`);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    }
+  }, []);
+  const handleClickTab = (
+    tab: string,
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setActiveTab(tab);
+    const section = document.querySelector(`#${tab}`);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+      setTimeout(() => {
+        window.location.hash = tab;
+      }, 1000);
+    }
+  };
   return (
     <>
       <div className="arlo_tm_leftpart_wrap">
@@ -14,19 +41,59 @@ const LeftPart = () => {
           <div className="menu_list_wrap">
             <ul className="anchor_nav">
               <li>
-                <a href="#home">Home</a>
+                <a
+                  href="#home"
+                  className={activeTab === "home" ? "active" : ""}
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => handleClickTab("home", event)}
+                >
+                  Home
+                </a>
               </li>
               <li>
-                <a href="#about">About</a>
+                <a
+                  href="#about"
+                  className={activeTab === "about" ? "active" : ""}
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => handleClickTab("about", event)}
+                >
+                  About
+                </a>
               </li>
               <li>
-                <a href="#skills">Skills</a>
+                <a
+                  href="#skills"
+                  className={activeTab === "skills" ? "active" : ""}
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => handleClickTab("skills", event)}
+                >
+                  Skills
+                </a>
               </li>
               <li>
-                <a href="#project">Projects</a>
+                <a
+                  href="#project"
+                  className={activeTab === "project" ? "active" : ""}
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => handleClickTab("project", event)}
+                >
+                  Projects
+                </a>
               </li>
               <li>
-                <a href="#contact">Contact</a>
+                <a
+                  href="#contact"
+                  className={activeTab === "contact" ? "active" : ""}
+                  onClick={(
+                    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+                  ) => handleClickTab("contact", event)}
+                >
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
@@ -34,18 +101,24 @@ const LeftPart = () => {
             <div className="social_wrap">
               <ul>
                 <li>
-                  <a href="https://www.facebook.com/khoi.donguyen.792" target="_blank">
+                  <a
+                    href="https://www.facebook.com/khoi.donguyen.792"
+                    target="_blank"
+                  >
                     <FaFacebook />
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.linkedin.com/in/nguy%C3%AAn-kh%C3%B4i-26032320b/" target="_blank">
+                  <a
+                    href="https://www.linkedin.com/in/nguy%C3%AAn-kh%C3%B4i-26032320b/"
+                    target="_blank"
+                  >
                     <FaLinkedin />
                   </a>
                 </li>
                 <li>
                   <a href="https://github.com/Khoi67" target="_blank">
-                  <FaGithub />
+                    <FaGithub />
                   </a>
                 </li>
               </ul>
